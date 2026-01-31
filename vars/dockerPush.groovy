16b170a493a41fd54947702a6c36ce453b1ab0e2
IkramAnsari01 <ikramansari21112001@gmail.com>
2026-01-31 15:27:35 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,11 +1,6 @@
-def call(String docCred, String image, String tag){
-  echo ' Pushing build Image to Docker Hub'
-  withCredentials([usernamePassword(
-          'credentialsId':${docCred},
-          passwordVariable: "docPass",
-          usernameVariable: "docUser")]){
-              sh "docker login -u ${env.docUser} -p ${env.docPass}" 
-              sh " docker image tag ${image}:${tag} ${env.docUser}/${image}:${tag}"
-              sh "docker push ${env.docUser}/notes-app:latest"
-          }
+def call(String docUser, String docPass, String image, String tag){
+ 
+    sh "docker login -u ${env.docUser} -p ${env.docPass}" 
+    sh " docker image tag ${image}:${tag} ${env.docUser}/${image}:${tag}"
+    sh "docker push ${env.docUser}/notes-app:latest"
 }
